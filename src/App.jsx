863aea34b55395956bab707ff72731e322aab80f
SREE-NIDHI89 <108682222+SREE-NIDHI89@users.noreply.github.com>
2023-09-22 10:11:08 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,72 @@
+// import logo from './logo.svg';
+import './App.css';
+import Register from './Components/Register';
+import Signup from './Components/Signup';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Showdata from './Components/Showdata';
+import Loginform from './Components/Login';
+import FlightBookingForm from './Components/FlightForm';
+
+
+function App() {
+  return (
+
+    <Router>
+      <div>
+        <Routes>
+    
+          <Route path='/' exact element={<Register/>}/>
+          <Route path='/login' element={<Loginform/>}/>
+         <Route path='/form' element={<FlightBookingForm/>}/>
+          </Routes>
+      </div>
+    </Router>
+
+   
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
+
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <div>
+//         <a href="https://vitejs.dev" target="_blank">
+//           <img src={viteLogo} className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://react.dev" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>Vite + React</h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//         <p>
+//           Edit <code>src/App.jsx</code> and save to test HMR
+//         </p>
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//     </>
+//   )
+// }
+
+// export default App
